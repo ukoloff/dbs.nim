78e93b97f84c1c222a141dc64b40297e9681d0c7
Stas Ukolov <ukoloff@gmail.com>
2020-08-13 22:12:15 +0500

    Read test data from file

--- tests/t_json.nim
+++ tests/t_json.nim
@@ -1,23 +1,9 @@
-import unittest, json, strutils
+import os, unittest, json, strutils
 
 import dbs/json as jjj
 
-let src = """
-[{
-  "partid": "ROUNDED3",
-  "paths": [
-  [
-  [0, 1, 0],
-  [0, 3, -0.414213],
-  [1, 4, 0],
-  [2, 4, -0.414213],
-  [3, 3, 0],
-  [3, 1, -0.414213],
-  [2, 0, 0],
-  [1, 0, -0.414213],
-  [0, 1, 0]]
-]}]
-""".strip
+const
+  src = currentSourcePath.parentDir.joinPath("../geodet/rounded3x4.json").staticRead.strip
 
 suite "JSON":
 
