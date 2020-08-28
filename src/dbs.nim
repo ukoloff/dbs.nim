44c84ae97c60f94d4ef778c19966472e2a147637
Stas Ukolov <ukoloff@gmail.com>
2020-08-28 22:29:31 +0300

    Extract CLI

--- src/dbs.nim
+++ src/dbs.nim
@@ -1,11 +1,8 @@
-# This is just an example to get you started. A typical library package
-# exports the main API in this file. Note that you cannot rename this file
-# but you can remove it if you wish.
-
 import dbs/dbs
 
 export DBS
 
 when isMainModule:
-  echo "int is ", int.sizeof,  " bits."
-  echo "ptr is ", (ptr int).sizeof,  " bits."
+  import dbs/private/cli
+
+  main()
---
+++ src/dbs/private/cli.nim
@@ -0,0 +1,3 @@
+proc main*() =
+  echo "int is ", int.sizeof,  " bits."
+  echo "ptr is ", (ptr int).sizeof,  " bits."
